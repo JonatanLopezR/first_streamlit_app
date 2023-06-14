b26aa77ac1fb277ca18c4c057d83d79a5dc624df
JonatanLopezR <136571310+JonatanLopezR@users.noreply.github.com>
2023-06-14 21:30:12 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -88,7 +88,7 @@ if streamlit.button('Get Fruit Load List'):
     streamlit.dataframe(my_data_rows)
 
 #don-t run anything past here while we troubleshoot
-streamlit.stop()
+#streamlit.stop()
 
 
 # Lets query snowflake
