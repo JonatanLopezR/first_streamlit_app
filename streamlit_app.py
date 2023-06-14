f2b912a811c65825916e5f2978905abba567df24
JonatanLopezR <136571310+JonatanLopezR@users.noreply.github.com>
2023-06-14 20:45:11 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -48,5 +48,5 @@ my_data_rows = my_cur.fetchall()
 streamlit.dataframe(my_data_rows)
 
 #combo para añadir otra fruta
-add_my_fruit = streamlit.text_input('What fruit would you like information about?', 'kiwi')
-streamlit.write('The user entered ', add_my_fruit)
+add_my_fruit = streamlit.text_input('What fruit would you like information about?', 'cereza')
+streamlit.write('Thanks for adding ', add_my_fruit)
